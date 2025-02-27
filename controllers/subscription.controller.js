955263d3432ffe13be05326e234763b79455a4c1
qnchoyy <qni191488@abv.bg>
2025-02-27 16:21:28 +0200

    Update subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,3 +1,6 @@
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
+
 import Subscription from "../models/Subscription.model.js";
 
 export const createSubscription = async (req, res, next) => {
@@ -7,16 +10,26 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id,
         });
 
-        res.status(201).json({ success: true, data: subscription });
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription.id,
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries: 0,
+        })
+
+        res.status(201).json({ success: true, data: subscription, workflowRunId });
     } catch (e) {
         next(e);
     }
 }
 
-
 export const getUserSubscriptions = async (req, res, next) => {
     try {
-        if (req.user.id !== req.params.id) {
+        if (req.user._id.toString() !== req.params.id) {
             const error = new Error('You are not the owner of this account');
             error.status = 401;
             throw error;
