ae2756ad0406c469ee578939e7e6ecea50336b0e
qnchoyy <qni191488@abv.bg>
2025-02-28 19:56:12 +0200

    Update subscription.routes.js

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import authorize from "../middlewares/auth.middleware.js";
-import { createSubscription, getUserSubscriptions } from "../controllers/subscription.controller.js";
+import { createSubscription, getUserSubscriptions, updateSubscription } from "../controllers/subscription.controller.js";
 
 const subscriptionRotuer = Router();
 
 // subscriptionRotuer.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }));
 // subscriptionRotuer.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }));
 subscriptionRotuer.post('/', authorize, createSubscription);
-// subscriptionRotuer.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
+subscriptionRotuer.put('/:id', authorize, updateSubscription);
 // subscriptionRotuer.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
 subscriptionRotuer.get('/user/:id', authorize, getUserSubscriptions);
 // subscriptionRotuer.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }));
