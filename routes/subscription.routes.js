ebc94b4589a293485251c7ed01400a82c55a83f7
qnchoyy <qni191488@abv.bg>
2025-03-02 22:00:55 +0200

    Update subscription.routes.js

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authorize from "../middlewares/auth.middleware.js";
-import { createSubscription, getSubscription, getUserSubscriptions, updateSubscription } from "../controllers/subscription.controller.js";
+import { createSubscription, deleteSubscription, getSubscription, getUserSubscriptions, updateSubscription } from "../controllers/subscription.controller.js";
 
 const subscriptionRotuer = Router();
 
@@ -8,7 +8,7 @@ const subscriptionRotuer = Router();
 subscriptionRotuer.get('/:id', authorize, getSubscription);
 subscriptionRotuer.post('/', authorize, createSubscription);
 subscriptionRotuer.put('/:id', authorize, updateSubscription);
-// subscriptionRotuer.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
+subscriptionRotuer.delete('/:id', authorize, deleteSubscription);
 subscriptionRotuer.get('/user/:id', authorize, getUserSubscriptions);
 // subscriptionRotuer.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }));
 // subscriptionRotuer.get('/upcoming-renewals', (req, res) => res.send({ title: 'GET upcoming renewals' }));
